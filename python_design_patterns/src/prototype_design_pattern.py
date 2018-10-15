c5823974fafbfadce51fbb6a4461a84a836aa391
maixiaochai <maixiaochai@qq.com>
2018-10-15 23:52:17 +0800

    +)prototype_design_pattern.py

---
+++ python_design_patterns/src/prototype_design_pattern.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# @File         : prototype_design_pattern.py
+# @Project      : mindmapping
+# @Time         : 2018/10/15 23:44
+# @Site :       : https://github.com/MaiXiaochai
+# @Author       : maixiaochai
+
+"""
+原型设计模式(Prototype design pattern)，无非就是克隆一个对象。
+如Python中的copy.deepcopy()实现的就是克隆一个对象。
+应用场景：我们已经有了一个对象，并希望创建对象的一个完整副本时。
+"""
