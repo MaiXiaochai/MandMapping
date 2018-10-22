71879eff302a9373eab47cd6230046df5c89a5eb
maixiaochai <maixiaochai@qq.com>
2018-10-23 00:36:08 +0800

    *)builder_pattern.py

--- python_design_patterns/src/builder_pattern.py
+++ python_design_patterns/src/builder_pattern.py
@@ -5,6 +5,9 @@
 # @Time         : 2018/10/3 23:53
 # @Author       : maixiaochai
 
+import time
+from enum import Enum
+
 # 建造者模式：
 # 将一个复杂对象的建造过程与其表现分离，这样，同一个构造过程可用于创建多个不同的表现。
 # 使用特征：一个对象必须经过多个步骤来创建，并且要求同一个构造过程可以产生不同的表现。
@@ -59,9 +62,22 @@ class AppleFactory:
         else:
             print("I don't know how to build {}".format(model))
 
-
+    """
+    工厂模式测试代码
+    afac = AppleFactory()
+    mac_mini = afac.builder_computer(MINI14)
+    print(mac_mini)
+    
+    Out:
+    Model: 1.4GHz Mac mini
+    Memory: 4GB
+    Hard Disk: 500GB
+    Graphics Card: Intel HD Graphics 5000
+    """
+# --------------------------------------------------------------------------------
 # 选择购买一台定制的PC。使用的是建造者模式。
 # 你是指挥者，向制造商（建造者）提供指令说明心中理想的电脑规格。
+
 
 class Computer:
     def __init__(self, serial_number):
@@ -113,14 +129,119 @@ def main():
     engineer.construct_computer(hdd=500, memory=8, gpu='GeForce GTX 650 Ti')
     computer = engineer.computer
     print(computer)
+    """
+    Out:
+    Model: AG23385193
+    Memory: 8GB
+    Hard Disk: 500GB
+    Graphics Card: GeForce GTX 650 T
+    """
+
+
+# --------------------------------------------------------------------------------
+# # 建造者应用：披萨订购应用
+"""
+准备好一个披萨，要经过遵从特定顺序的许多步骤。
+要添加调味料，得先准备生面团，
+要添加配料，得先添加调味料，
+要烤披萨，得添加了调味料和配料，
+要烤好，得取决于生面团的厚度和配料。
+生面团 -> 调味料 -> 配料 -> 烤（厚度，配料） 
+"""
+
+# 声明一些Enum参数
+PizzaProgress = Enum('PizzaProgress', 'queued preparation baking ready')
+
+# dough 生面团； thin 薄的； thick 厚的
+PizzaDough = Enum('PizzaDough', 'thin thick')
+
+# sauce 沙司，酱汁， 给 ... 调味； tomato 番茄；potato 土豆； creme fraiche 鲜奶油
+PizzaSauce = Enum('PizzaSauce', 'tomato creme_fraiche')
+
+# topping 蛋糕上的装饰配料； mozarella 奶酪，干酪； bacon 培根，熏肉； ham 火腿； mushroom 蘑菇；red onion 红皮洋葱；
+# oregano 牛至叶，止痢草、土香薷、小叶薄荷
+PizzaTopping = Enum('PizzaTopping', 'mozzarella double_mozzarella bacon ham mushroom red_onion oregano')
+
+# 各个步骤间的延迟
+STEP_DELAY = 3
+
+
+class Pizza:
+    """
+    最终的产品是一个披萨，由Pizza类描述，若使用建造者模式，则最终产品（类）并没有多少职责，因为他不支持直接实例化。
+    建造者会直接创建一个最终产品的实例，并确保这个实例万全准备好。这就是Pizza类这么小的缘由。它只是将所有数据初始化为合理的默认值，
+    唯一的例外方法是prepare_dough。将该方法定义在Pizza类而不是建造者中，是考虑到以下两点：
+        1）为了澄清一点，就是虽然最终产品类通常会最小化，但这并不意味着绝不应该给它分配任何职责；
+        2）为了通过组合提高代码复用。
+    """
+    def __init__(self, name):
+        self.name = name
+        self.dough = None
+        self.sauce = None
+        self.topping = []
+
+    def __str__(self):
+        return self.name
+
+    def prepare_dough(self, dough):
+        self.dough = dough
+        print('preparing the {} dough of your {} ...'.format(self.dough.name, self))
+        time.sleep(STEP_DELAY)
+        print('done with the {} dough'.format(self.dough.name))
+
+
+"""
+两个建造者，一个制作玛格丽特披萨（MargaritaBuilder）,另一个制作奶油熏肉披萨（CreamyBaconBuilder）。
+每个建造者都创建一个Pizza实例，并包含遵从披萨制作流程的方法：prepare_dough()、add_sauce、add_topping()和bake()。
+准确说，其中的prepare_dough只是对Pizza类中prepare_dough()方法的一层封装。注意每个建造者是如何处理所有披萨相关细节的。
+"""
+
+
+class MargaritaBuilder:
+    """
+    玛格丽特披萨
+    生面团：薄
+    调味料：番茄酱
+    配料：双层马苏里拉奶酪（mozzarella）、牛至
+    烘焙：5s
+
+    """
+    def __init__(self):
+        self.pizza = Pizza('margarita')
+        self.progress = PizzaProgress.queued
+        self.baking_time = 5
+
+    def prepare_dough(self):
+        self.progress = PizzaProgress.preparation
+        self.pizza.prepare_dough(PizzaDough.thin)
+
+    def add_sauce(self):
+        print('adding the tomato sauce to your matgarita ...')
+        time.sleep(STEP_DELAY)
+        print('done with the tomato sauce')
+
+    def add_topping(self):
+        print('adding the topping (double mozzarella, oregano) to your margarita')
+        self.pizza.topping.append([i for i in (PizzaTopping.double_mozzarella, PizzaTopping.oregano)])
+        time.sleep(STEP_DELAY)
+        print('done with the topping (double mozzarella, oregano)')
+
+    def bake(self):
+        self.progress = PizzaProgress.baking
+        print('baking your margarita for {} seconds'.format(self.baking_time))
+        time.sleep(self.baking_time)
+
+
+class CreamyBaconBuilder:
+    def __init__(self):
+        self.pizza = Pizza('Creamy bacon')
+        self.progress = PizzaProgress.queued
+        self.baking_time = 7
+
+    def prepare_dough(self):
+        pass
+        # to be continued ...
 
 
 if __name__ == '__main__':
-    # 工厂模式测试代码
-    # afac = AppleFactory()
-    # mac_mini = afac.builder_computer(MINI14)
-    # print(mac_mini)
     pass
-    main()
-
-
