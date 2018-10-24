ccc22b5974fc79bd42cf63c7762313c84dcd6be1
maixiaochai <maixiaochai@qq.com>
2018-10-25 00:12:32 +0800

    +)adapter_pattern.py

---
+++ python_design_patterns/src/adapter_pattern.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# @File         : adapter_pattern.py
+# @Project      : MindMapping
+# @Time         : 2018/10/24 23:45
+# @Site :       : https://github.com/MaiXiaochai
+# @Author       : maixiaochai
+
+"""
+适配器模式：在两个接口外使得两个接口能正常通信的代码层。
+"""
