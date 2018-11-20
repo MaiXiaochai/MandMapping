2cd1606968e29d2748201edaa04b751989da56f5
maixiaochai <maixiaochai@qq.com>
2018-11-20 23:55:31 +0800

    proxy.py done

--- python_design_patterns/src/proxy.py
+++ python_design_patterns/src/proxy.py
@@ -39,12 +39,41 @@ class LazyProperty:
         """
         __get__()方法所访问的特性值，正是下层方法想要赋的值，并用setattr()来手动赋值。
         __get__()实际做的事非常简单，就是使用值来替代方法！这意味着不仅特性是惰性加载的，而且仅可以设置一次。
+
+        # ----------------------------------------------------------------------------------------------
+        关于__get__():
+            object.__get__(self, instance, owner)
+            如果class定义了它，则这个class就可以称为descriptor。
+            owner是所有者的类，instance是访问descriptor的实例，如果不是通过实例访问，而是通过类访问的话，instance则为None。
+            （descriptor的实例自己访问自己是不会触发__get__，而会触发__call__，只有descriptor作为其它类的属性才有意义。）
+
+        关于setattr():
+            setattr(object, name, values)
+            给对象的属性赋值，若属性不存在，先创建再赋值
+            class test():
+            ...     name='Tom'
+            ...     def run(self):
+            ...             return 'HelloWord'
+            ...
+            t=test()
+            hasattr(t, "age")   #判断属性是否存在
+            out:False
+
+            setattr(t, "age", "18")   #为属相赋值，并没有返回值
+            hasattr(t, "age")    #属性存在了
+            Out:True
+
+            有点烧脑，暂时略过整个案例。
+            更多关于__get__ 和__set__的详细说明，请阅读
+            https://blog.csdn.net/leafage_m/article/details/54960432
+
         """
+
         if not obj:
             return None
 
         value = self.method(obj)
-        # print('value {}'.format(value))
+        print('value {}'.format(value))
         setattr(obj, self.method_name, value)
         return value
 
@@ -91,5 +120,79 @@ def main():
     print(t.resource)
 
 
+# ---------------------------------------------------------------------------------------------------------------------
+"""
+现实生活中的例子：
+芯片（又名芯片密码）卡是现实生活中使用防护代理的一个好例子。
+借记/信用卡包含一个芯片，ATM机或读卡器需要先读取芯片；在芯片通过验证后，需要一个密码（PIN）才能完成交易。
+这意味着只有在物理地提供芯片卡且知道密码时才能进行交易。
+
+使用银行指派哦代替现金进行购买和交易是远程代理的一个例子。
+支票准许了对一个银行账户的访问。
+
+对象关系映射（Object-Relational Mapping, ORM）API是一个如何使用远程代理的例子。
+
+
+下面将实现一个简单的保护代理来查看和添加用户。该服务提供一下两个选项：
+    1）查看用户列表：这一操作不需要特许权限；
+    2）添加新用户：这一操作要求客户端提供一个特殊的密码。
+    
+"""
+
+
+class SensitiveInfo:
+    def __init__(self):
+        self.users = ['nick', 'tom', 'ben', 'mike']
+
+    def read(self):
+        print('There are {} users: {}'.format(len(self.users), ' '.join(self.users)))
+
+    def add(self, user):
+        self.users.append(user)
+        print('Added user {}'.format(user))
+
+
+class Info:
+    """
+    Info 是SensitiveInfo的一个保护代理。secret变量值是客户端代码在添加新用户时被要求告知/提供的密码。
+    注意，在实际应用中永远不要执行以下操作：
+        1）在源码中存储密码；
+        2）以明文形式存储密码；
+        3）使用一种弱（例如，MD5）货自定义加密形式。
+    """
+    def __init__(self):
+        self.protected = SensitiveInfo()
+        self.secret = '0xdeadbeef'
+
+    def read(self):
+        self.protected.read()
+
+    def add(self, user):
+        sec = input('What is the secret? ')
+        self.protected.add(user) if sec == self.secret else print("That's wrong!")
+
+
+def secret_main():
+    info = Info()
+
+    while True:
+        print("1. read list |==| 2. add user |==| 3. quit")
+        key = input('choose option: ')
+
+        if key == '1':
+            info.read()
+
+        elif key == '2':
+            name = input('choose username: ')
+            info.add(name)
+
+        elif key == '3':
+            exit()
+
+        else:
+            print('unknown option: {}'.format(key))
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    secret_main()
