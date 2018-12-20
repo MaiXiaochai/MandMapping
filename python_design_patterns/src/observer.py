21a6a237978c205eaaab95984342e0d598214717
maixiaochai <maixiaochai@qq.com>
2018-12-21 00:09:50 +0800

    +)observer.py.

---
+++ python_design_patterns/src/observer.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# @File         : observer.py
+# @Project      : src
+# @Time         : 2018/12/21 0:09
+# @Site         : https://github.com/MaiXiaochai
+# @Author       : maixiaochai
+
